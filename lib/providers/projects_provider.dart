473eac62512f4d2b47e958451121d245d467e556
Luu95 <lucas@dolz.de>
2026-05-01 20:28:58 +0200

    Revert "ui angepasst, logiken verbessert"
    This reverts commit a843a0f17838dfe54180630847184d1f41c972b3.

--- lib/providers/projects_provider.dart
+++ lib/providers/projects_provider.dart
@@ -12,7 +12,7 @@ class ProjectsState {
   final bool isLoading;
   final int? selectedProjectIndex;
   final int? selectedBuildingIndex;
-
+//huhn
   ProjectsState({
     required this.projects,
     this.isLoading = false,
